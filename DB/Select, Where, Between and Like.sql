6b102bdf95c2577c625e61044cbcd89ec5932e27
uiuxmathesh <matheshpremkumar@gmail.com>
2024-04-27 17:26:19 +0530

    "Updation"

--- DB/Select, Where, Between and Like.sql
+++ DB/Select, Where, Between and Like.sql
@@ -3,3 +3,16 @@ INSERT INTO [students] ([student_id],[first_name],[last_name],[email],[date_of_b
 
 INSERT INTO [enrollments]([enrollment_id],[student_id],[course_id],[enrollment_date])
 	VALUES ('E0011','S0011','C0008','2024-02-03')
+
+UPDATE [teacher]
+SET [email] = 'prithvirajsukumaran21@gmail.com'
+WHERE [teacher_id] = 'T0004'
+
+UPDATE [course]
+SET [teacher_id] = 'T0010'
+WHERE [course_id] = 'C0010'
+
+
+UPDATE [payments]
+SET [amount] = 2250.0
+WHERE [payment_id] = 'P0006'
